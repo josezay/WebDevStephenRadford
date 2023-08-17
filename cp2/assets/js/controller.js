3a94f35cae254df208ebe7cd677698c167912c15
José Zay <josezay22@gmail.com>
2023-08-16 21:36:55 -0300

    ng-class test

--- cp2/assets/js/controller.js
+++ cp2/assets/js/controller.js
@@ -1,5 +1,18 @@
 angular.module('myApp', [])
 .controller('AppCtl', function($scope) {
+    $scope.contacts = [
+        {
+            name: 'John Doe',
+            phone: '01234567890',
+            email: 'john@example.com'
+        },
+        {
+            name: 'Karan Bromwich',
+            phone: '09876543210',
+            email: 'karan@email.com'
+        }
+    ];
+
     $scope.clickHandler = function() {
         $scope.isHidden = !$scope.isHidden;
     };
